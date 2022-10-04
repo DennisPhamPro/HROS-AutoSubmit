756e10325bfafe6513a19ee08934074acdcb8e1f
Dennis_Pham <dennis.pham@terralogic.com>
2022-10-04 15:43:56 +0700

    check

--- login-submit.py
+++ login-submit.py
@@ -1,11 +1,13 @@
 import pandas as pd
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import chromedriver_autoinstaller
-import logging 
+import logging
 ######################################################
 chrome_options = Options()
 #chrome_options.add_argument("--incognito")
